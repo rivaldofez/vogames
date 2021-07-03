cf839dc0733e33fb8c0d1a84726e3969b9809acf
rivaldofez <rivaldofez@gmail.com>
2021-07-04 02:32:57 +0700

    add fragment and setting jetpack navigation

--- app/src/main/java/com/rivaldofez/vogames/HomeActivity.kt
+++ app/src/main/java/com/rivaldofez/vogames/HomeActivity.kt
@@ -2,10 +2,33 @@ package com.rivaldofez.vogames
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.rivaldofez.vogames.databinding.ActivityHomeBinding
+import com.rivaldofez.vogames.favorite.FavoriteFragment
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        binding.bnavMain.setNavigationChangeListener{_, position ->
+            when(position){
+                0 -> {
+                    navController.navigate(R.id.gamesFragment)
+                }
+                1 -> {
+                    navController.navigate(R.id.favoriteFragment)
+                }
+                2 -> {
+                    navController.navigate(R.id.aboutFragment)
+                }
+            }
+        }
     }
 }

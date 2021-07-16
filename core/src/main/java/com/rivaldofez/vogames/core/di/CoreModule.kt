e1528a27c5b0cd1f517b4aaf8e2ceddbd05b949a
rivaldofez <rivaldofez@gmail.com>
2021-07-17 02:07:12 +0700

    implement ssl pinner

--- core/src/main/java/com/rivaldofez/vogames/core/di/CoreModule.kt
+++ core/src/main/java/com/rivaldofez/vogames/core/di/CoreModule.kt
@@ -10,6 +10,7 @@ import com.rivaldofez.vogames.core.domain.repository.IGameRepository
 import com.rivaldofez.vogames.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,10 +21,17 @@ import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
+        val hostname = "api.rawg.io"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/hS5jJ4P+iQUErBkvoWBQOd1T7VOAYlOVegvv1iMzpxA=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .build()
+
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
